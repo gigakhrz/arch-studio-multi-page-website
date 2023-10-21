628f7c041779b0dc69717147484d99b52dcc6830
gigakhrz <kharazishvilig13@gmail.com>
2023-10-21 16:13:49 +0400

    add logo img and burger menu

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,9 +1,14 @@
 import logoImg from "../../../../public/assets/logo.svg";
-
+import burgerImg from "../../../../public/assets/icons/icon-hamburger.svg";
 export default function Header(): JSX.Element {
   return (
-    <div className="flex w-full justify-between p-8">
-      <img src={logoImg.src} alt="logoImg" />
+    <div className="flex w-full justify-between p-8 items-center">
+      <img className="w-[77px] h-[32px]" src={logoImg.src} alt="logoImg" />
+      <img
+        className="w-[24px] h-[17px]"
+        src={burgerImg.src}
+        alt="burger menu"
+      />
     </div>
   );
 }
