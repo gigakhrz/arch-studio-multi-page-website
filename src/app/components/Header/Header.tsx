a5bcad6af8e816f2f3b040f6aad594fbd13459af
gigakhrz <kharazishvilig13@gmail.com>
2023-10-21 16:37:00 +0400

    add handlegohompage function using useRouter

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,9 +1,23 @@
+"use client";
 import logoImg from "../../../../public/assets/logo.svg";
 import burgerImg from "../../../../public/assets/icons/icon-hamburger.svg";
+import { useRouter } from "next/navigation";
+
 export default function Header(): JSX.Element {
+  const router = useRouter();
+  // to go home page when click on logo img
+  const handleGoHomePage = (): void => {
+    router.push("/");
+  };
+
   return (
     <div className="flex w-full justify-between p-8 items-center">
-      <img className="w-[77px] h-[32px]" src={logoImg.src} alt="logoImg" />
+      <img
+        className="w-[77px] h-[32px]"
+        src={logoImg.src}
+        alt="logoImg"
+        onClick={handleGoHomePage}
+      />
       <img
         className="w-[24px] h-[17px]"
         src={burgerImg.src}
