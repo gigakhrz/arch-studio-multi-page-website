84ededef97973533ac34b7a2bfb3bbc00950f519
gigakhrz <kharazishvilig13@gmail.com>
2023-10-22 10:59:26 +0400

    create burger menu component

---
+++ src/app/components/Header/BurgerMenu.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+const Burgermenu = (): JSX.Element => {
+  return (
+    <div className="w-[343px] flex flex-col items-center px-12 py-[39px]"></div>
+  );
+};
+
+export default Burgermenu;
