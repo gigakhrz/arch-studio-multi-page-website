c101e5829da77673e0817e2908b564d598365f05
gigakhrz <kharazishvilig13@gmail.com>
2023-10-26 21:20:51 +0400

    create home second child taht is welcomome and import it in home component

---
+++ src/app/components/homeChilds/Welocome.tsx
@@ -0,0 +1,5 @@
+const Welcome = (): JSX.Element => {
+  return <div className="flex flex-col gap-[69px]"></div>;
+};
+
+export default Welcome;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Paramour from "./components/homeChilds/Paramour";
+import Welcome from "./components/homeChilds/Welocome";
 
 export default function Home() {
   return (
     <div className="flex gap-[72px] items-center flex-col">
       <Paramour />
+      <Welcome />
     </div>
   );
 }
