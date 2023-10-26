7e3d712ec7e8f959ab2348152227729863428007
gigakhrz <kharazishvilig13@gmail.com>
2023-10-26 21:58:33 +0400

    create h1 in featured

--- src/app/components/homeChilds/Feature.tsx
+++ src/app/components/homeChilds/Feature.tsx
@@ -1,5 +1,11 @@
 const Feature = (): JSX.Element => {
-  return <></>;
+  return (
+    <div className="flex flex-col items-center gap-[43px] w-full px-[32px]">
+      <h1 className="text-[48px] leading-[52px] tracking-[-1.7px] text-[#1B1D23] self-start font-bold">
+        Featured
+      </h1>
+    </div>
+  );
 };
 
 export default Feature;
