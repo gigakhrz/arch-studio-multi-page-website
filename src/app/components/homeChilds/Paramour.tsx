98f1fb46d2d3536ab85a43387d7440b827ecc817
gigakhrz <kharazishvilig13@gmail.com>
2023-10-26 15:23:20 +0400

    add overlay

--- src/app/components/homeChilds/Paramour.tsx
+++ src/app/components/homeChilds/Paramour.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import ParamourImg from "../../../../public/assets/home/mobile/image-hero-paramour.jpg";
 
 const Paramour = (): JSX.Element => {
@@ -8,12 +9,23 @@ const Paramour = (): JSX.Element => {
   };
 
   return (
-    <div className="w-full flex flex-col py-[113px] px-8" style={paramourStyle}>
-      <div className="flex flex-col items-center w-full justify-center">
-        <h1 className="text-5xl leading-12 text-white self-center">
+    <div
+      className="w-full flex flex-col py-[113px] px-8 relative gap-[83px]"
+      style={paramourStyle}
+    >
+      <div className=" z-10 flex flex-col items-center w-full justify-center gap-[11px]">
+        <h1 className="text-[48px] leading-[48px] tracking-[-1.2px] text-white self-center font-bold">
           Project Paramour
         </h1>
+        <p className="text-[18px] leading-[24px] font-medium text-white ">
+          Project made for an art museum near Southwest London. Project Paramour
+          is a statement of bold, modern architecture.
+        </p>
       </div>
+
+      <Link href="/portfolio"></Link>
+
+      <div className="w-full h-full bg-black opacity-50 absolute top-0 left-0"></div>
     </div>
   );
 };
