98886a2cf874c5816ef875bffc858b50922a1cd8
gigakhrz <kharazishvilig13@gmail.com>
2023-10-26 14:59:08 +0400

    add heading

--- src/app/components/homeChilds/Paramour.tsx
+++ src/app/components/homeChilds/Paramour.tsx
@@ -1,12 +1,21 @@
 import ParamourImg from "../../../../public/assets/home/mobile/image-hero-paramour.jpg";
 
 const Paramour = (): JSX.Element => {
+  // styles taht can't add using tailwind
   const paramourStyle = {
-    backgroundImage: `url(${ParamourImg})`,
+    backgroundImage: `url(${ParamourImg.src})`,
     backgroundSize: "cover",
   };
 
-  return <div style={paramourStyle}></div>;
+  return (
+    <div className="w-full flex flex-col py-[113px] px-8" style={paramourStyle}>
+      <div className="flex flex-col items-center w-full justify-center">
+        <h1 className="text-5xl leading-12 text-white self-center">
+          Project Paramour
+        </h1>
+      </div>
+    </div>
+  );
 };
 
 export default Paramour;
