b5ac3ad5dc3ecc669315b5e77c240614598ec521
gigakhrz <kharazishvilig13@gmail.com>
2023-10-26 23:15:18 +0400

    create features child some design

--- src/app/components/homeChilds/Feature.tsx
+++ src/app/components/homeChilds/Feature.tsx
@@ -1,9 +1,12 @@
+import FeaturesChilds from "./FeaturesChilds";
+
 const Feature = (): JSX.Element => {
   return (
     <div className="flex flex-col items-center gap-[43px] w-full px-[32px]">
       <h1 className="text-[48px] leading-[52px] tracking-[-1.7px] text-[#1B1D23] self-start font-bold">
         Featured
       </h1>
+      <FeaturesChilds />
     </div>
   );
 };
--- src/app/components/homeChilds/FeaturesChilds.tsx
+++ src/app/components/homeChilds/FeaturesChilds.tsx
@@ -1,24 +1,44 @@
 const FeaturesChilds = (): JSX.Element => {
-  //features thats will map in jsx
-  const features = [
-    {
-      title: "Project Del Sol",
-      img: "",
-    },
-    {
-      title: "228B Tower",
-      img: "",
-    },
-    {
-      title: "Le Prototype",
-      img: "",
-    },
-  ];
   return (
-    <div className="flex flex-col items-center justify-center gap-[24px]">
-      {features.map((feature) => {
-        return <div></div>;
-      })}
+    <div className="flex flex-col w-full gap-[24px]">
+      <div className="w-[311px] h-[240px] flex flex-col items-start justify-end px-[24px] py-[23px] relative   bg-projectMob-img bg-no-repeat	bg-cover">
+        <h1 className=" z-10 text-[32px] leading-[40px]  text-white self-start font-bold">
+          Project Del Sol
+        </h1>
+        <h3 className=" z-10 text-[18px] leading-[24px]  text-white self-start font-medium opacity-75">
+          View All Projects
+        </h3>
+
+        {/* for overlay */}
+        <div className="w-full h-full bg-black opacity-50 absolute top-0 left-0"></div>
+      </div>
+
+      {/* --------------- */}
+
+      <div className="w-[311px] h-[240px] flex flex-col items-start justify-end px-[24px] py-[23px] relative   bg-towerMob-img bg-no-repeat	bg-cover">
+        <h1 className=" z-10 text-[32px] leading-[40px]  text-white self-start font-bold">
+          228B Tower
+        </h1>
+        <h3 className=" z-10 text-[18px] leading-[24px]  text-white self-start font-medium opacity-75">
+          View All Projects
+        </h3>
+        {/* for overlay */}
+        <div className="w-full h-full bg-black opacity-50 absolute top-0 left-0"></div>
+      </div>
+
+      {/* --------------- */}
+
+      <div className="w-[311px] h-[240px] flex flex-col items-start justify-end px-[24px] py-[23px] relative   bg-protorypeMob-img bg-no-repeat	bg-cover">
+        <h1 className=" z-10 text-[32px] leading-[40px]  text-white self-start font-bold">
+          Le Prototype
+        </h1>
+        <h3 className=" z-10 text-[18px] leading-[24px]  text-white self-start font-medium opacity-75">
+          View All Projects
+        </h3>
+
+        {/* for overlay */}
+        <div className="w-full h-full bg-black opacity-50 absolute top-0 left-0"></div>
+      </div>
     </div>
   );
 };
