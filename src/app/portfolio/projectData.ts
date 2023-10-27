0dc2215931ca550c0be322495af2ac5063757953
gigakhrz <kharazishvilig13@gmail.com>
2023-10-27 11:19:15 +0400

    create aray for portfolio infromations

---
+++ src/app/portfolio/projectData.ts
@@ -0,0 +1,66 @@
+export const allProject = [
+  {
+    name: "Seraph Station",
+    date: "September 2019",
+    image: "/assets/portfolio/mobile/image-eebox.jpg",
+  },
+  {
+    name: "Eebox Building",
+    date: "August 2017",
+    image: "/assets/portfolio/mobile/image-seraph.jpg",
+  },
+
+  {
+    name: "Federal II Tower",
+    date: "March 2017",
+    image: "/assets/portfolio/mobile/image-federal.jpg",
+  },
+
+  {
+    name: "Project Del Sol",
+    date: "January 2016",
+    image: "/assets/portfolio/mobile/image-del-sol.jpg",
+  },
+
+  {
+    name: "Le Prototype",
+    date: "October 2015",
+    image: "/assets/portfolio/mobile/image-prototype.jpg",
+  },
+
+  {
+    name: "228B Tower",
+    date: "April 2015",
+    image: "/assets/portfolio/mobile/image-228b.jpg",
+  },
+
+  {
+    name: "Grand Edelweiss Hote",
+    date: "December 2013",
+    image: "/assets/portfolio/mobile/image-edelweiss.jpg",
+  },
+
+  {
+    name: "Hypers",
+    date: "January 2012",
+    image: "/assets/portfolio/mobile/image-hypers.jpg",
+  },
+
+  {
+    name: "SXIV Tower",
+    date: "March 2011",
+    image: "/assets/portfolio/mobile/image-sxiv.jpg",
+  },
+
+  {
+    name: "Trinity Bank Tower",
+    date: "September 2010",
+    image: "/assets/portfolio/mobile/image-trinity.jpg",
+  },
+
+  {
+    name: "Project Paramour",
+    date: "February 2008",
+    image: "/assets/portfolio/mobile/image-paramour.jpg",
+  },
+];
