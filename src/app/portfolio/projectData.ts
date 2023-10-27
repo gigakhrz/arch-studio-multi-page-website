14e8ac9ee376bca8355a6b927462b21d9eba38ed
gigakhrz <kharazishvilig13@gmail.com>
2023-10-27 11:50:34 +0400

    add tablet and sektop images

--- src/app/portfolio/projectData.ts
+++ src/app/portfolio/projectData.ts
@@ -3,6 +3,8 @@ interface Projects {
     name: string;
     date: string;
     image: string;
+    image1: string;
+    image2: string;
   }[];
 }
 
@@ -12,65 +14,87 @@ export const allProject: Projects = {
       name: "Seraph Station",
       date: "September 2019",
       image: "/assets/portfolio/mobile/image-eebox.jpg",
+      image1: "/assets/portfolio/tablet/image-eebox.jpg",
+      image2: "/assets/portfolio/desktop/image-eebox.jpg",
     },
     {
       name: "Eebox Building",
       date: "August 2017",
       image: "/assets/portfolio/mobile/image-seraph.jpg",
+      image1: "/assets/portfolio/tablet/image-seraph.jpg",
+      image2: "/assets/portfolio/desktop/image-seraph.jpg",
     },
 
     {
       name: "Federal II Tower",
       date: "March 2017",
       image: "/assets/portfolio/mobile/image-federal.jpg",
+      image1: "/assets/portfolio/tablet/image-federal.jpg",
+      image2: "/assets/portfolio/desktop/image-federal.jpg",
     },
 
     {
       name: "Project Del Sol",
       date: "January 2016",
       image: "/assets/portfolio/mobile/image-del-sol.jpg",
+      image1: "/assets/portfolio/tablet/image-del-sol.jpg",
+      image2: "/assets/portfolio/desktop/image-del-sol.jpg",
     },
 
     {
       name: "Le Prototype",
       date: "October 2015",
       image: "/assets/portfolio/mobile/image-prototype.jpg",
+      image1: "/assets/portfolio/tablet/image-prototype.jpg",
+      image2: "/assets/portfolio/desktop/image-prototype.jpg",
     },
 
     {
       name: "228B Tower",
       date: "April 2015",
       image: "/assets/portfolio/mobile/image-228b.jpg",
+      image1: "/assets/portfolio/tablet/image-228b.jpg",
+      image2: "/assets/portfolio/desktop/image-228b.jpg",
     },
 
     {
       name: "Grand Edelweiss Hote",
       date: "December 2013",
       image: "/assets/portfolio/mobile/image-edelweiss.jpg",
+      image1: "/assets/portfolio/tablet/image-edelweiss.jpg",
+      image2: "/assets/portfolio/desltop/image-edelweiss.jpg",
     },
 
     {
       name: "Hypers",
       date: "January 2012",
       image: "/assets/portfolio/mobile/image-hypers.jpg",
+      image1: "/assets/portfolio/tablet/image-hypers.jpg",
+      image2: "/assets/portfolio/desktop/image-hypers.jpg",
     },
 
     {
       name: "SXIV Tower",
       date: "March 2011",
       image: "/assets/portfolio/mobile/image-sxiv.jpg",
+      image1: "/assets/portfolio/tablet/image-sxiv.jpg",
+      image2: "/assets/portfolio/desktop/image-sxiv.jpg",
     },
 
     {
       name: "Trinity Bank Tower",
       date: "September 2010",
       image: "/assets/portfolio/mobile/image-trinity.jpg",
+      image1: "/assets/portfolio/tablet/image-trinity.jpg",
+      image2: "/assets/portfolio/desktop/image-trinity.jpg",
     },
 
     {
       name: "Project Paramour",
       date: "February 2008",
       image: "/assets/portfolio/mobile/image-paramour.jpg",
+      image1: "/assets/portfolio/tablet/image-paramour.jpg",
+      image2: "/assets/portfolio/desktop/image-paramour.jpg",
     },
   ],
 };
