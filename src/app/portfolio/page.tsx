2b4d9b3a10195d73cf424a91d65b479fb7515344
gigakhrz <kharazishvilig13@gmail.com>
2023-10-28 20:42:58 +0400

    add coment

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -18,6 +18,7 @@ export default function Portfolio() {
     };
   }, []);
 
+  //   function that catch responsive increation to change project image based on this
   function getDeviceType() {
     const width = window.innerWidth;
 
