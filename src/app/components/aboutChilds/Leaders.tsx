187f3c9d6c4909d074754bdee2ec263546b0d092
gigakhrz <kharazishvilig13@gmail.com>
2023-10-31 10:30:11 +0400

    add leaders object and his interface

--- src/app/components/aboutChilds/Leaders.tsx
+++ src/app/components/aboutChilds/Leaders.tsx
@@ -1,4 +1,39 @@
 const Leadrs = (): JSX.Element => {
+  interface Leaders {
+    leaders: {
+      fullName: string;
+      position: string;
+      img: string;
+    }[];
+  }
+
+  const teamLeaders: Leaders = {
+    leaders: [
+      {
+        fullName: "Jake Richards",
+        position: "Chief Architect",
+        img: "/assets/about/desktop/avatar-jake.jpg",
+      },
+
+      {
+        fullName: "Thompson Smith",
+        position: "Head of Finance",
+        img: "/assets/about/desktop/avatar-thompson.jpg",
+      },
+
+      {
+        fullName: "Jackson Rourke",
+        position: "Lead Designer",
+        img: "/assets/about/desktop/avatar-jackson.jpg",
+      },
+
+      {
+        fullName: "Maria Simpson",
+        position: "Senior Architect",
+        img: "/assets/about/desktop/avatar-maria.jpg",
+      },
+    ],
+  };
   return <></>;
 };
 
