acc827952c1dd834c1a74e8dd58a30d81e3b0946
gigakhrz <kharazishvilig13@gmail.com>
2023-10-31 10:19:40 +0400

    create Leadrs wich is about child and impor tit in about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,4 +1,5 @@
 import Heritage from "../components/aboutChilds/Heritage";
+import Leadrs from "../components/aboutChilds/Leaders";
 import Team from "../components/aboutChilds/Team";
 
 const About = (): JSX.Element => {
@@ -6,6 +7,7 @@ const About = (): JSX.Element => {
     <div className="w-full flex flex-col items-center gap-[68px] mb-[171px]">
       <Team />
       <Heritage />
+      <Leadrs />
     </div>
   );
 };
---
+++ src/app/components/aboutChilds/Leaders.tsx
@@ -0,0 +1,5 @@
+const Leadrs = (): JSX.Element => {
+  return <></>;
+};
+
+export default Leadrs;
