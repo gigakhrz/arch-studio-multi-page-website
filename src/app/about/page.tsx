7bc3d24c1dad6c0292ec513278f21baeb159a46b
gigakhrz <kharazishvilig13@gmail.com>
2023-10-31 10:09:21 +0400

    add Heritage compoennt and import it in about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,11 @@
+import Heritage from "../components/aboutChilds/Heritage";
 import Team from "../components/aboutChilds/Team";
 
 const About = (): JSX.Element => {
   return (
     <div className="w-full flex flex-col items-center gap-[68px] mb-[171px]">
       <Team />
+      <Heritage />
     </div>
   );
 };
