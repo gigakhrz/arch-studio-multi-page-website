a38d61df0b8e2a06dd9bcaab48e45245dd29daec
gigakhrz <kharazishvilig13@gmail.com>
2023-10-31 13:34:38 +0400

    add details

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,5 +1,11 @@
+import ContactDetails from "../components/contacChilds/Details";
 import TellUs from "../components/contacChilds/TellUs";
 
 export default function Contact(): JSX.Element {
-  return <TellUs />;
+  return (
+    <div className="w-full flex flex-col items-center gap-[68px] mb-[171px]">
+      <TellUs />;
+      <ContactDetails />
+    </div>
+  );
 }
