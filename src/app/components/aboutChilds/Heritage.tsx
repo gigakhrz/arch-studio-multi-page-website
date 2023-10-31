c53d491aae61b3e6cf1f24f362ebff5309c73bda
gigakhrz <kharazishvilig13@gmail.com>
2023-10-31 10:14:33 +0400

    add corect texts

--- src/app/components/aboutChilds/Heritage.tsx
+++ src/app/components/aboutChilds/Heritage.tsx
@@ -1,27 +1,25 @@
 const Heritage = (): JSX.Element => {
   return (
     <div className="flex w-[375px] items-center px-[32px]">
-      <div className=" flex flex-col items-center gap-[22px] ">
+      <div className=" flex flex-col items-start gap-[22px] ">
         <h1 className="text-[48px] leading-[52px] font-bold tracking=[-1.72px] text-[#1B1D23]">
-          Welcome to Arch Studio
+          Our <br /> Heritage
         </h1>
         <div className="flex flex-col items-center gap-[30px] mb-[40px]">
           <p className="text-[18px] leading-[24px] font-medium  text-[#60636D]">
-            We have a unique network and skillset to help bring your projects to
-            life. Our small team of highly skilled individuals combined with our
-            large network put us in a strong position to deliver exceptional
-            results.
+            Founded in 2007, we started as a trio of architects. Our
+            complimentary skills and relentless attention to detail turned Arch
+            into one of the most sought after boutique firms in the country.
           </p>
           <p className="text-[18px] leading-[24px] font-medium  text-[#60636D]">
-            Over the past 10 years, we have worked on all kinds of projects.
-            From stations to high-rise buildings, we create spaces that inspire
-            and delight.
+            Speciliazing in Urban Design allowed us to focus on creating
+            exceptional structures that live in harmony with their surroundings.
+            We consider every detail from every surrounding element to inform
+            our designs.
           </p>
           <p className="text-[18px] leading-[24px] font-medium  text-[#60636D]">
-            We work closely with our clients so that we understand the
-            intricacies of each project. This allows us to work in harmony the
-            surrounding area to create truly stunning projects that will stand
-            the test of time.
+            Our small team of world-class professionals provides input on every
+            project.
           </p>
         </div>
       </div>
