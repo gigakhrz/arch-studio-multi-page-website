de97b6885c985846c6280f0a6a18c8167112d748
gigakhrz <kharazishvilig13@gmail.com>
2023-10-31 13:20:02 +0400

    add overlay and bottom white rectangle

--- src/app/components/aboutChilds/Team.tsx
+++ src/app/components/aboutChilds/Team.tsx
@@ -2,10 +2,13 @@ import aboutImg from "../../../../public/assets/about/mobile/image-hero.jpg";
 
 const Team = (): JSX.Element => {
   return (
-    <div className="flex w-[375px] flex-col items-start justify-center gap-[20px] px-[32px]">
-      <div className="relative w-full h-[240px] bg-aboutMob bg-no-repeat	bg-cover"></div>
+    <div className="flex w-[375px] flex-col items-start justify-center gap-[20px] ">
+      <div className="relative w-full h-[240px] bg-aboutMob bg-no-repea bg-cover flex items-end justify-start ">
+        <div className="bg-white w-[343px] h-[45px] z-10"></div>
+        <div className="bg-black absolute w-full h-full left-0 top-0 opacity-50"></div>
+      </div>
 
-      <div className="flex flex-col gap-[76px] items-start">
+      <div className="flex flex-col gap-[76px] items-start px-[32px]">
         <div className="flex flex-col gap-[21px] w-full">
           <h1 className="text-[48px] leading-[52px] font-bold tracking=[-1.2px] text-[#1B1D23]">
             Your team of professionals
