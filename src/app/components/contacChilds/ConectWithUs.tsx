0ba1d79e95b1d58e5e5f902c0b5f3ee7e44cc74f
gigakhrz <kharazishvilig13@gmail.com>
2023-10-31 13:38:53 +0400

    add conect with us

---
+++ src/app/components/contacChilds/ConectWithUs.tsx
@@ -0,0 +1,46 @@
+import arrowSvg from "../../../../public/assets/icons/icon-white-arrow.svg";
+
+export default function ConnectWith() {
+  return (
+    <div className="w-[375px] px-[32px] gap-[42px] flex flex-col mb-[132px]">
+      <h2 className="text-[48px] text-[#1B1D23] w-[200px] font-bold leading-[52px] tracking-[-1.714px]">
+        Connect with us
+      </h2>
+      <form className="w-full flex flex-col gap-[43px]">
+        <div className="flex flex-col w-full gap-[21px]">
+          <input
+            type="text"
+            placeholder="Name"
+            className="pl-[34px] text-[20px] text-[#1B1D23] font-bold leading-normal tracking-[ -0.312px] placeholder-[#C8CCD8] outline-none"
+          />
+          <hr className="h-[1px] border-none bg-[#1B1D23]" />
+        </div>
+
+        <div className="flex flex-col w-full gap-[21px]">
+          <input
+            type="email"
+            placeholder="Email"
+            className="pl-[34px] text-[20px] text-[#1B1D23] font-bold leading-normal tracking-[ -0.312px] placeholder-[#C8CCD8] outline-none"
+          />
+          <hr className="h-[1px] border-none bg-[#1B1D23]" />
+        </div>
+        <div className="w-full flex flex-col items-end">
+          <div className="flex flex-col w-full gap-[21px]">
+            <input
+              type="text"
+              placeholder="Message"
+              className="pl-[34px] text-[20px] text-[#1B1D23] font-bold leading-normal tracking-[ -0.312px] placeholder-[#C8CCD8] outline-none"
+            />
+            <hr className="h-[1px] border-none bg-[#1B1D23]" />
+          </div>
+          <button
+            type="submit"
+            className="w-[80px] h-[80px] bg-[#1B1D23] flex justify-center items-center cursor-pointer"
+          >
+            <img src={arrowSvg.src} />
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
