05d207ac88aedf66700355387b2270dfd6fb9402
gigakhrz <kharazishvilig13@gmail.com>
2023-11-05 20:40:01 +0400

    add Links in nav bar

--- src/app/components/Header/Navbar.tsx
+++ src/app/components/Header/Navbar.tsx
@@ -1,5 +1,28 @@
+import Link from "next/link";
+
 const Navbar = (): JSX.Element => {
-  return <></>;
+  return (
+    <div className="w-full flex gap-[61px] items-start">
+      <Link
+        href="/portfolio"
+        className="text-[#7D828F] text-[32px] leading-[40px] font-bold"
+      >
+        Portfolio
+      </Link>
+      <Link
+        href="/about"
+        className="text-[#7D828F] text-[32px] leading-[40px] font-bold"
+      >
+        About Us
+      </Link>
+      <Link
+        href="/contact"
+        className="text-[#7D828F] text-[32px] leading-[40px] font-bold"
+      >
+        Contact
+      </Link>
+    </div>
+  );
 };
 
 export default Navbar;
