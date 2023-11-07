9e19e263640c87c65403f3d1bc862174e4794be5
gigakhrz <kharazishvilig13@gmail.com>
2023-11-07 21:07:31 +0400

    create tablet header styles

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -3,17 +3,20 @@ import logoImg from "../../../../public/assets/icons/Logo.svg";
 import arrowIcon from "../../../../public/assets/icons/Group 18.svg";
 const Footer = (): JSX.Element => {
   return (
-    <div className="flex flex-col items-center justify-center gap-[32px] bg-[#EEEFF4]">
-      <img src={logoImg.src} alt="logo image" className="mt-[-60px]" />
+    <div
+      className="flex flex-col items-center justify-center gap-[32px] bg-[#EEEFF4] md:flex-row md:h-[120px] md:justify-start md:w-[612px] 
+    md:gap-[40px] "
+    >
+      <img src={logoImg.src} alt="logo image" className="mt-[-60px] md:m-0" />
       <Link
-        className="text-[#7D828F] text-[18px] font-bold leading-[25px] "
+        className="text-[#7D828F] text-[18px] font-bold leading-[25px] md:mr-[21px] "
         href="/portfolio"
       >
         Portfolio
       </Link>
 
       <Link
-        className="text-[#7D828F] text-[18px] font-bold leading-[25px] "
+        className="text-[#7D828F] text-[18px] font-bold leading-[25px] md:mr-[11px]"
         href="/about"
       >
         About us
@@ -27,7 +30,7 @@ const Footer = (): JSX.Element => {
       </Link>
 
       <Link
-        className="flex items-center justify-center gap-[24px] bg-black z-10 w-[252px] h-[72px]"
+        className="flex items-center justify-center gap-[24px] bg-black z-10 w-[252px] h-[72px] min-w-[252px] md:mr-[-300px]"
         href="/portfolio"
       >
         <h4 className="text-[18px] leading-[25px] font-bolds text-white ">
