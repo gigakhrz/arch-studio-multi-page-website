6e1b9ebb7b03d4f0c2c22dd0c2b1d8625940a591
gigakhrz <kharazishvilig13@gmail.com>
2023-11-07 13:42:30 +0400

    add new button wich will display on tablet

--- src/app/components/homeChilds/Feature.tsx
+++ src/app/components/homeChilds/Feature.tsx
@@ -4,13 +4,25 @@ import arrowIcon from "../../../../public/assets/icons/Group 18.svg";
 
 const Feature = (): JSX.Element => {
   return (
-    <div className="flex flex-col items-start gap-[43px] w-[375px] px-[32px]">
-      <h1 className="text-[48px] leading-[52px] tracking-[-1.7px] text-[#1B1D23] self-start font-bold">
-        Featured
-      </h1>
+    <div className="flex flex-col items-start gap-[43px] w-[375px] px-[32px] md:w-[573px] md:p-0">
+      <div className="flex items-center justify-between w-full">
+        <h1 className="text-[48px] leading-[52px] tracking-[-1.7px] text-[#1B1D23] self-start font-bold md:self-center">
+          Featured
+        </h1>
+
+        <Link
+          className="hidden items-center justify-center gap-[24px] bg-black z-10 w-[252px] h-[72px] md:flex"
+          href="/portfolio"
+        >
+          <h4 className="text-[18px] leading-[25px] font-bolds text-white ">
+            See all
+          </h4>
+          <img src={arrowIcon.src} alt="arrow img" />
+        </Link>
+      </div>
       <FeaturesChilds />
       <Link
-        className="flex items-center justify-center gap-[24px] bg-black z-10 w-[252px] h-[72px]"
+        className="flex items-center justify-center gap-[24px] bg-black z-10 w-[252px] h-[72px] md:hidden"
         href="/portfolio"
       >
         <h4 className="text-[18px] leading-[25px] font-bolds text-white ">
