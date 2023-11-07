65d9ec1ac058f1ac7a41e7e562339cb528ac48e4
gigakhrz <kharazishvilig13@gmail.com>
2023-11-07 13:14:28 +0400

    add home's about tablet styles

--- src/app/components/homeChilds/About.tsx
+++ src/app/components/homeChilds/About.tsx
@@ -3,13 +3,20 @@ import arrowIcon from "../../../../public/assets/icons/Group 18.svg";
 
 const About = (): JSX.Element => {
   return (
-    <div className="w-[375px] flex flex-col items-start justify-center px-8 relative gap-[23px] h-[560px] bg-aboutMob-img bg-no-repeat	bg-cover">
-      <h1 className=" z-10 text-[48px] leading-[48px] tracking-[-1.2px] text-white self-center font-bold">
-        Small team, big ideas
+    <div
+      className="w-[375px] flex flex-col items-start justify-center px-8 relative gap-[23px] h-[560px] bg-aboutMob-img bg-no-repeat	bg-cover 
+    md:w-[573px] md:h-[560px] md:pl-[57px]"
+    >
+      <h1
+        className=" z-10 text-[48px] leading-[48px] tracking-[-1.2px] text-white self-start font-bold md:text-[72px]  
+        md:leading-[64px] md:tracking-[-2px] "
+      >
+        Small team,
+        <br /> big ideas
       </h1>
 
       <Link
-        className="flex items-center justify-center gap-[24px] bg-black z-10 w-[252px] h-[72px]"
+        className="flex items-center justify-center gap-[24px] bg-black z-10 w-[252px] h-[72px] md:w-[187px]"
         href="/about"
       >
         <h4 className="text-[18px] leading-[25px] font-bolds text-white ">
