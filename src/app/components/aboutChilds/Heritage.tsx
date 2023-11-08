25fb2a0fb3e6214024ca85749927f069e9b600dc
gigakhrz <kharazishvilig13@gmail.com>
2023-11-08 11:12:31 +0400

    heritage responcive

--- src/app/components/aboutChilds/Heritage.tsx
+++ src/app/components/aboutChilds/Heritage.tsx
@@ -1,8 +1,12 @@
 const Heritage = (): JSX.Element => {
   return (
-    <div className="flex w-[375px] items-center px-[32px] mb-[43px]">
-      <div className=" flex flex-col items-start gap-[22px] ">
-        <h1 className="text-[48px] leading-[52px] font-bold tracking=[-1.72px] text-[#1B1D23]">
+    <div className="flex w-[375px] items-center px-[32px] mb-[43px] md:w-[574px] md:p-0">
+      <div className=" flex flex-col items-start gap-[22px] md:gap-[50px]">
+        <hr className="hidden bg-[#C8CCD8] border-none w-[65px] h-[1px] md:mt-[85px] md:block" />
+        <h1
+          className="text-[48px] leading-[52px] font-bold tracking=[-1.72px] text-[#1B1D23]  md:text-[72px]  
+        md:leading-[64px] md:tracking-[-2px] md:w-[446px] md:mb-[10px]"
+        >
           Our <br /> Heritage
         </h1>
         <div className="flex flex-col items-center gap-[30px] mb-[40px]">
