df53e90b210c749811c9179438520eab2d8188fe
gigakhrz <kharazishvilig13@gmail.com>
2023-11-08 10:17:41 +0400

    add tablet design

--- src/app/components/aboutChilds/Team.tsx
+++ src/app/components/aboutChilds/Team.tsx
@@ -2,13 +2,16 @@ import aboutImg from "../../../../public/assets/about/mobile/image-hero.jpg";
 
 const Team = (): JSX.Element => {
   return (
-    <div className="flex w-[375px] flex-col items-start justify-center gap-[20px] ">
-      <div className="relative w-full h-[240px] bg-aboutMob bg-no-repea bg-cover flex items-end justify-start ">
-        <div className="bg-white w-[343px] h-[45px] z-10"></div>
+    <div className="flex w-[375px] flex-col items-start justify-center gap-[20px] md:w-[573px]	md:relative ">
+      <div className="relative w-full h-[240px] bg-aboutMob bg-no-repeat bg-cover flex items-end justify-start md:w-[573px] md:h-[760px]">
+        <div className="bg-white w-[343px] h-[45px] z-10 md:hidden"></div>
         <div className="bg-black absolute w-full h-full left-0 top-0 opacity-50"></div>
       </div>
 
-      <div className="flex flex-col gap-[76px] items-start px-[32px]">
+      <div
+        className="flex flex-col gap-[76px] items-start px-[32px] md:absolute md:bg-[#FFFFFF] md:w-[515px] md:bottom-[-1px] md:right-[-1px] 
+      md:pl-[59px] md:pr-0 md:flex-col-reverse"
+      >
         <div className="flex flex-col gap-[21px] w-full">
           <h1 className="text-[48px] leading-[52px] font-bold tracking=[-1.2px] text-[#1B1D23]">
             Your team of professionals
@@ -21,7 +24,7 @@ const Team = (): JSX.Element => {
           </p>
         </div>
 
-        <hr className="bg-[#C8CCD8] border-none w-[65px] h-[1px] " />
+        <hr className="bg-[#C8CCD8] border-none w-[65px] h-[1px] md:mt-[85px]" />
       </div>
     </div>
   );
