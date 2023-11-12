fa5c4a45e5dc3e4b6d2a7546d0715f030f0f1a5a
gigakhrz <kharazishvilig13@gmail.com>
2023-11-12 11:26:14 +0400

    make flex row and increase height

--- src/app/components/homeChilds/FeaturesChilds.tsx
+++ src/app/components/homeChilds/FeaturesChilds.tsx
@@ -2,10 +2,10 @@ import Link from "next/link";
 
 const FeaturesChilds = (): JSX.Element => {
   return (
-    <div className="flex flex-col w-full gap-[24px] ">
+    <div className="flex flex-col w-full gap-[24px] lg:flex-row lg:gap-[30px]">
       <div
         className="w-[311px] h-[240px] flex flex-col items-start justify-end px-[24px] py-[23px] relative   bg-projectMob-img bg-no-repeat	bg-cover 
-        md:w-full md:pl-[40px] md:pb-[40px] md:bg-projectTab-img
+        md:w-full md:pl-[40px] md:pb-[40px] md:bg-projectTab-img lg:h-[560px] 
       "
       >
         <h1 className=" z-10 text-[32px] leading-[40px]  text-white self-start font-bold">
