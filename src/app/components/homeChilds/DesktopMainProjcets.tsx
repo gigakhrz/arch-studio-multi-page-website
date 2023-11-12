ee318773d42f102b1d4f9d9fb07989427c3379ce
gigakhrz <kharazishvilig13@gmail.com>
2023-11-12 10:15:25 +0400

    update styles

--- src/app/components/homeChilds/DesktopMainProjcets.tsx
+++ src/app/components/homeChilds/DesktopMainProjcets.tsx
@@ -4,33 +4,47 @@ import Image from "next/image";
 
 const DesktopMainProjects = (): JSX.Element => {
   return (
-    <div className="w-full flex flex-col gap-[22px] px-[32px] items-start mb-[113px] relative md:gap-[43px] md:w-[572px] md:px-0 md:mb-[233px] lg:flex-row lg:w-[1110px] lg:items-end lg:justify-center lg:gap-[125px]">
-      <h3 className="hidden md:flex absolute top-[-150px] w-[581px] text-[120px] font-bold text-[#EEEFF4] leading-[200px] tracking-[-3px] lg:text-[250px] lg:left-[-150px] lg:top-[-90px]">
-        Welcome
-      </h3>
-      <div className="flex flex-col gap-[22px] md:gap-[43px]">
-        <h1 className="text-[48px] font-bold text-[#1B1D23] leading-[52px] tracking-[-1.714px] md:text-[72px] md:leading-[64px] md:tracking-[-2px] md:w-[446px]">
-          Welcome to Arch Studio
-        </h1>
-        <div className="flex flex-col gap-[22px] md:w-[572px] lg:w-[445px]">
-          <p className="text-[18px] font-medium text-[#60636D] leading-[24px]">
-            We have a unique network and skillset to help bring your projects to
-            life. Our small team of highly skilled individuals combined with our
-            large network put us in a strong position to deliver exceptional
-            results.
-          </p>
-          <p className="text-[18px] font-medium text-[#60636D] leading-[24px]">
-            Over the past 10 years, we have worked on all kinds of projects.
-            From stations to high-rise buildings, we create spaces that inspire
-            and delight.
-          </p>
-          <p className="text-[18px] font-medium text-[#60636D] leading-[24px]">
-            We work closely with our clients so that we understand the
-            intricacies of each project. This allows us to work in harmony the
-            surrounding area to create truly stunning projects that will stand
-            the test of time.
-          </p>
-        </div>
+    <div
+      className="hidden lg:flex w-full h-[560px] bg-no-repeat bg-cover px-[32px] justify-center items-start flex-col gap-[83px] relative mb-[141px] md:w-[573px] md:h-[720px] md:px-[58px] md:gap-[41px] md:mb-[248px] lg:w-[1110px] lg:px-[190px]"
+      style={{ backgroundImage: `url()` }}
+    >
+      <div className="w-full flex flex-col gap-[11px] items-start md:gap-[10px]">
+        <h1 className="text-[48px] text-[#FFF] font-bold leading-[48px] tracking-[-1.2px] z-10 md:text-[96px] md:leading-[80px] lg:w-[544px]"></h1>
+        <p className="text-[18px] text-[#FFF] font-medium leading-[24px] z-10 lg:w-[445px]"></p>
+      </div>
+      <Link
+        href="/Portfolio"
+        className="w-[252px] cursor-pointer h-[72px] flex justify-center items-center gap-[24px] bg-[#1B1D23] text-[#FFFFFF] text-[18px] font-bold leading-[25px] z-10 hover:bg-[#60636D]"
+      >
+        See Our Portfolio
+        <Image src={arrowIcon} alt="arrow svg" />
+      </Link>
+      <div className="absolute w-full h-full bg-black opacity-50 top-0 left-0"></div>
+      <div className="absolute w-[320px] flex flex-row items-center z-20 left-[-80px] bottom-0">
+        <button
+          className={`flex items-center justify-center  w-[80px] h-[80px] cursor-pointer hover:bg-[#EEEFF4] focus:bg-[#1B1D23] focus:text-[#FFFFFF] `}
+          value={"01"}
+        >
+          01
+        </button>
+        <button
+          className="flex items-center justify-center bg-[#FFFFFF] text-[#7D828F] w-[80px] h-[80px] cursor-pointer hover:bg-[#EEEFF4] focus:bg-[#1B1D23] focus:text-[#FFFFFF]"
+          value={"02"}
+        >
+          02
+        </button>
+        <button
+          className="flex items-center justify-center bg-[#FFFFFF] text-[#7D828F] w-[80px] h-[80px] cursor-pointer hover:bg-[#EEEFF4] focus:bg-[#1B1D23] focus:text-[#FFFFFF]"
+          value={"03"}
+        >
+          03
+        </button>
+        <button
+          className="flex items-center justify-center bg-[#FFFFFF] text-[#7D828F] w-[80px] h-[80px] cursor-pointer hover:bg-[#EEEFF4] focus:bg-[#1B1D23] focus:text-[#FFFFFF]"
+          value={"04"}
+        >
+          04
+        </button>
       </div>
     </div>
   );
