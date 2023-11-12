4b36e2de7c3cca5f3e15f2f01d6d7cb79da46bf1
gigakhrz <kharazishvilig13@gmail.com>
2023-11-12 11:30:56 +0400

    changee bgs

--- src/app/components/homeChilds/FeaturesChilds.tsx
+++ src/app/components/homeChilds/FeaturesChilds.tsx
@@ -5,7 +5,7 @@ const FeaturesChilds = (): JSX.Element => {
     <div className="flex flex-col w-full gap-[24px] lg:flex-row lg:gap-[30px]">
       <div
         className="w-[311px] h-[240px] flex flex-col items-start justify-end px-[24px] py-[23px] relative   bg-projectMob-img bg-no-repeat	bg-cover 
-        md:w-full md:pl-[40px] md:pb-[40px] md:bg-projectTab-img lg:h-[560px] 
+        md:w-full md:pl-[40px] md:pb-[40px] md:bg-projectTab-img lg:h-[560px] lg:bg-projectDes-img
       "
       >
         <h1 className=" z-10 text-[32px] leading-[40px]  text-white self-start font-bold">
@@ -26,7 +26,7 @@ const FeaturesChilds = (): JSX.Element => {
 
       <div
         className="w-[311px] h-[240px] flex flex-col items-start justify-end px-[24px] py-[23px] relative   bg-towerMob-img bg-no-repeat	bg-cover
-      md:w-full md:pl-[40px] md:pb-[40px] md:bg-towerTab-img  "
+      md:w-full md:pl-[40px] md:pb-[40px] md:bg-towerTab-img  lg:h-[560px] lg:bg-towerDes-img"
       >
         <h1 className=" z-10 text-[32px] leading-[40px]  text-white self-start font-bold">
           228B Tower
@@ -45,7 +45,7 @@ const FeaturesChilds = (): JSX.Element => {
 
       <div
         className="w-[311px] h-[240px] flex flex-col items-start justify-end px-[24px] py-[23px] relative   bg-protorypeMob-img bg-no-repeat	bg-cover 
-      md:w-full md:pl-[40px] md:pb-[40px] md:bg-protorypeTab-img"
+      md:w-full md:pl-[40px] md:pb-[40px] md:bg-protorypeTab-img lg:h-[560px] lg:bg-protorypeDes-img"
       >
         <h1 className=" z-10 text-[32px] leading-[40px]  text-white self-start font-bold">
           Le Prototype
