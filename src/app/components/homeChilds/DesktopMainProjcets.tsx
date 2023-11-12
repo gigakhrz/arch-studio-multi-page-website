b31179cecbb51e0f8f1330c8ddfc44fa3c492d3a
gigakhrz <kharazishvilig13@gmail.com>
2023-11-12 10:29:24 +0400

    delete unnecesary files

--- src/app/components/homeChilds/DesktopMainProjcets.tsx
+++ src/app/components/homeChilds/DesktopMainProjcets.tsx
@@ -27,11 +27,11 @@ const DesktopMainProjects = () => {
   }, [itemNum]);
   return (
     <div
-      className="hidden lg:flex w-full h-[560px] bg-no-repeat bg-cover px-[32px] justify-center items-start flex-col gap-[83px] relative mb-[141px] md:w-[573px] md:h-[720px] md:px-[58px] md:gap-[41px] md:mb-[248px] lg:w-[1110px] lg:px-[190px]"
+      className="hidden lg:flex w-full bg-no-repeat bg-cover  justify-center items-start flex-col relative md:h-[720px] md:gap-[41px] md:mb-[248px] lg:w-[1110px] lg:px-[190px]"
       style={{ backgroundImage: `url(${project[0].image})` }}
     >
-      <div className="w-full flex flex-col gap-[11px] items-start md:gap-[10px]">
-        <h1 className="text-[48px] text-[#FFF] font-bold leading-[48px] tracking-[-1.2px] z-10 md:text-[96px] md:leading-[80px] lg:w-[544px]">
+      <div className="w-full flex flex-col  items-start md:gap-[10px]">
+        <h1 className=" text-[#FFF] font-bold  z-10 md:text-[96px] md:leading-[80px] lg:w-[544px]">
           {project[0].name}
         </h1>
         <p className="text-[18px] text-[#FFF] font-medium leading-[24px] z-10 lg:w-[445px]">
