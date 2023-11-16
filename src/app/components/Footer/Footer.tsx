19af8c1d56a8dd722fbfd16f475a24529d48c70d
gigakhrz <kharazishvilig13@gmail.com>
2023-11-16 18:09:07 +0400

    add headers hovers

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -14,28 +14,28 @@ const Footer = (): JSX.Element => {
         className="mt-[-60px] md:m-0 lg:w-[200px] h-[200px]"
       />
       <Link
-        className="text-[#7D828F] text-[18px] font-bold leading-[25px] md:mr-[21px] lg:ml-[24px]"
+        className="text-[#7D828F] text-[18px] font-bold leading-[25px] md:mr-[21px] lg:ml-[24px]  lg:hover:text-[#1B1D23]"
         href="/portfolio"
       >
         Portfolio
       </Link>
 
       <Link
-        className="text-[#7D828F] text-[18px] font-bold leading-[25px] md:mr-[11px]"
+        className="text-[#7D828F] text-[18px] font-bold leading-[25px] md:mr-[11px]  lg:hover:text-[#1B1D23]"
         href="/about"
       >
         About us
       </Link>
 
       <Link
-        className="text-[#7D828F] text-[18px] font-bold leading-[25px] "
+        className="text-[#7D828F] text-[18px] font-bold leading-[25px]  lg:hover:text-[#1B1D23]"
         href="/contact"
       >
         Contact
       </Link>
 
       <Link
-        className="flex items-center justify-center gap-[24px] bg-black z-10 w-[252px] h-[72px] min-w-[252px] md:mr-[-300px] lg:absolute lg:right-[180px]"
+        className="flex items-center justify-center gap-[24px] bg-black z-10 w-[252px] h-[72px] min-w-[252px] md:mr-[-300px] lg:absolute lg:right-[180px]  lg:hover:bg-[#60636D]"
         href="/portfolio"
       >
         <h4 className="text-[18px] leading-[25px] font-bolds text-white ">
