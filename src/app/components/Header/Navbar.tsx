96e048f7909b039866ddfe661f0b2c2c7b5a5202
gigakhrz <kharazishvilig13@gmail.com>
2023-11-16 18:05:22 +0400

    add the navbar hovers

--- src/app/components/Header/Navbar.tsx
+++ src/app/components/Header/Navbar.tsx
@@ -5,19 +5,19 @@ const Navbar = (): JSX.Element => {
     <div className="w-full  gap-[61px] items-center hidden md:flex">
       <Link
         href="/portfolio"
-        className="text-[#7D828F] text-[18px] leading-[25px] font-bold"
+        className="text-[#7D828F] text-[18px] leading-[25px] font-bold lg:hover:text-[#1B1D23]"
       >
         Portfolio
       </Link>
       <Link
         href="/about"
-        className="text-[#7D828F] text-[18px] leading-[25px] font-bold"
+        className="text-[#7D828F] text-[18px] leading-[25px] font-bold  lg:hover:text-[#1B1D23]"
       >
         About Us
       </Link>
       <Link
         href="/contact"
-        className="text-[#7D828F] text-[18px] leading-[25px] font-bold"
+        className="text-[#7D828F] text-[18px] leading-[25px] font-bold  lg:hover:text-[#1B1D23]"
       >
         Contact
       </Link>
